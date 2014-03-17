1a33fc103d748319d07cf1afba2af2d12f882a92
Jordann <jordan.cz@gmail.com>
2014-03-17 12:17:07 -0400

    added jobs

--- public/js/counter.js
+++ public/js/counter.js
@@ -1,27 +1,40 @@
- var myModule = angular.module("counterapp", []);
+var myModule = angular.module("counterapp", []);
 
 myModule.controller("counterCtrl",['$scope','$timeout', function($scope,$timeout){
-
- //Adding initial value for counter
   
-$scope.counter = 0;
-var stopped;
-
-//timeout function is 1000ms = 1 second
-  
-$scope.start = function() {
-    stopped = $timeout(function() {
-       console.log($scope.counter);
-     $scope.counter++;   
-     $scope.start();   
-    }, 1000);
-  };
-   
+    //Adding initial value for counter
     
-$scope.stop = function(){
-   $timeout.cancel(stopped);
+    $scope.counter = 0;
+    var stopped;
+    //timeout function is 1000ms = 1 second
     
+    $scope.start = function() {
+	stopped = $timeout(function() {
+	console.log($scope.counter);
+	$scope.counter++;   
+	$scope.start();   
+	}, 1000);
+    };
+    
+	
+    $scope.stop = function(){
+    $timeout.cancel(stopped);
     } 
 
+    $scope.reset = function(){
+	$timeout.cancel(stopped);
+	$scope.counter = 0;
+    }
 
 }]);
+
+function JobsListCtrl ($scope) {
+     $scope.jobs = [
+	  { "name": "job1",
+	    "duration": 10},
+	  { "name": "job2",
+	    "duration": 20},
+	  { "name": "job3",
+	    "duration": 30}
+	  ];
+}
