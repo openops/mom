8d3283ef2a192b74be6908c71e747e922a8c37d0
Jordann <jordan.cz@gmail.com>
2014-07-20 19:48:19 -0400

    fixing jade problem

--- app.js
+++ app.js
@@ -13,14 +13,11 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 8000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
