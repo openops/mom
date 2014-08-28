8b681304f47ea678402cfd540d0080939d2cb765
Jordann <jordan.cz@gmail.com>
2014-08-28 11:19:07 -0400

    changing structure

---
+++ public/js/newtimer.js
@@ -0,0 +1,17 @@
+var Checkups = Backbone.model.extend({
+    initialize: function() {
+    
+    },
+});
+
+var Activites = Backbone.model.extend({
+    initialize: funtction() {
+
+    },
+});
+
+var Timers = Backbone.model.extend({
+    initialize: function(){
+
+    },
+});
